8c81c1a5fc102d52cdbb5d9707669389d3e4b2b7
GWTTKB <harrelsonleland@gmail.com>
2026-04-27 20:34:27 -0400

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,8 +1,8 @@
 const BASE = 'https://github.com/nflverse/nflverse-data/releases/download';
 
 const FILES = {
-  player_stats_week:   (season) => `${BASE}/stats_player/stats_player_week_${season}.csv`,
-  player_stats_reg:    (season) => `${BASE}/stats_player/stats_player_reg_${season}.csv`,
+  player_stats_week:   (season) => `${BASE}/player_stats/player_stats_${season}.csv`,
+  player_stats_reg:    (season) => `${BASE}/player_stats/player_stats_${season}.csv`,
   player_stats_post:   (season) => `${BASE}/stats_player/stats_player_post_${season}.csv`,
   snap_counts:         (season) => `${BASE}/snap_counts/snap_counts_${season}.csv`,
   injuries:            (season) => `${BASE}/injuries/injuries_${season}.csv`,
@@ -10,9 +10,9 @@ const FILES = {
   pfr_pass:            (season) => `${BASE}/pfr_advstats/advstats_season_pass_${season}.csv`,
   pfr_rush:            (season) => `${BASE}/pfr_advstats/advstats_season_rush_${season}.csv`,
   pfr_rec:             (season) => `${BASE}/pfr_advstats/advstats_season_rec_${season}.csv`,
-  ngs_pass:            (season) => `${BASE}/nextgen_stats/ngs_${season}_passing.csv`,
-  ngs_rush:            (season) => `${BASE}/nextgen_stats/ngs_${season}_rushing.csv`,
-  ngs_rec:             (season) => `${BASE}/nextgen_stats/ngs_${season}_receiving.csv`,
+  ngs_pass:            (season) => `${BASE}/nextgen_stats/nextgen_stats_passing_${season}.csv`,
+  ngs_rush:            (season) => `${BASE}/nextgen_stats/nextgen_stats_rushing_${season}.csv`,
+  ngs_rec:             (season) => `${BASE}/nextgen_stats/nextgen_stats_receiving_${season}.csv`,
   players:             () => `${BASE}/players/players.csv`,
 };
 
